d6a2cd632d36542857d6ee2f38393b60f71d8a39
andre <andre@acs-andre.acs.co.il>
2014-03-17 15:13:39 +0200

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml
    	public_html/index.html

---
+++ public_html/pro1/app/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var appModule = angular.module('app', []);
+appModule.directive('ngbkFocus', function() {
+    return {
+        link: function(scope, element, attrs, controller) {
+            element[0].focus();
+        }
+    };
+});
